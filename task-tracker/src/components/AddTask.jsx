d6bc2b90529c8c011aee5f63727e7a14ed764c5e
Allan Bifom (aeb672) <aeb672@usask.ca>
2025-01-11 01:42:59 -0600

    Added a modal for creating Task, more to come

--- task-tracker/src/components/AddTask.jsx
+++ task-tracker/src/components/AddTask.jsx
@@ -1,13 +1,33 @@
 import React from 'react'
+import { useState } from 'react'
 
 function AddTask() {
+    const [addModal, setAddModal] = useState(false);
   return (
     <div>
-        <button>
-            +New
+        <button className='bg-blue-500 text-white uppercase text-sm font-semibold py-1 mx-1.5 pl-2 pr-2.5 rounded hover:opacity-70'
+                type = 'button'
+                onClick={() => setAddModal(true)}
+        >
+            + New
         </button>
+        {addModal ? (
+            <div>
+                <div className='flex items-center justify-center overflow-x-hidden overflow-y-auto fixed inset-0 z-100'>
+                    <div className='w-9/12 flex flex-row justify-between p-5 border bg-white'>
+                        <h3 className='text-3xl font-semibold'>Add new Task</h3>
+                        <button className='px-1 text-gray-400 float-right text-3xl leading-none font-semibold block'
+                                onClick={() => setAddModal(false)}
+                        >
+                            X
+                        </button>
+                    </div>    
+                    
+                </div>
+            </div>
+        ) : null}
     </div>
-  )
+    )
 }
 
-export default AddTask
+export default AddTask;
