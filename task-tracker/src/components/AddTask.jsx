8560efece7e99773ac0d0ec7cc5fcabbc2c1c91d
Allan Bifom (aeb672) <aeb672@usask.ca>
2025-01-12 02:18:13 -0600

    started an input form to get Add Task

--- task-tracker/src/components/AddTask.jsx
+++ task-tracker/src/components/AddTask.jsx
@@ -14,13 +14,27 @@ function AddTask() {
         {addModal ? (
             <div>
                 <div className='flex items-center justify-center overflow-x-hidden overflow-y-auto fixed inset-0 z-100'>
-                    <div className='w-9/12 flex flex-row justify-between p-5 border bg-white'>
-                        <h3 className='text-3xl font-semibold'>Add new Task</h3>
-                        <button className='px-1 text-gray-400 float-right text-3xl leading-none font-semibold block'
+                    <div className='w-9/12 bg-white rounded-lg shadow-md relative flex flex-col'>
+                        <div className='flex flex-row justify-between p-5'>
+                            <h3 className='text-3xl font-semibold'>Add new Task</h3>
+                            <button className='px-1 text-gray-400 float-right text-3xl leading-none font-semibold block'
                                 onClick={() => setAddModal(false)}
-                        >
-                            X
-                        </button>
+                            >
+                                X
+                            </button>
+                        </div>
+                        <form className='p-6'>
+                            <label>
+                                Task Name
+                            </label>
+                            <input
+                            className='w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-5 leading-tight focus:outline-none focus:bg-white'
+                                id="task-name"
+                                type='text'
+                                placeholder='Task name'
+                                required
+                            />
+                        </form>
                     </div>    
                     
                 </div>
