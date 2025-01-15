f990529a399613ed628b2c6f8db79074e3cd07cd
Allan Bifom (aeb672) <aeb672@usask.ca>
2025-01-14 23:57:54 -0600

    finished UI for ToDo.jsx

--- task-tracker/src/components/ToDo.jsx
+++ task-tracker/src/components/ToDo.jsx
@@ -4,12 +4,22 @@ function ToDo({task, key}) {
 return (
     <div>
         <div className='flex flex-col items-start justify-start bg-white my-4 ml-6 py-4 px-6 w-3/4 max-w-lg'>
-            <p className='font-semibold text-lg'>
-                {task.taskName}
-            </p>
+            <div className='w-full flex flex-row justify-between'>
+                <p className='font-semibold text-lg'>
+                    {task.taskName}
+                </p>
+                <button>
+                    Edit
+                </button>
+            </div>
             <p className='text-lg py-2'>
                 {task.taskDescription}
             </p>
+            <div className='w-full flex flex-row justify-end'>
+                <button className='bg-red-500 text-white text-sm font-semibold py-1.5 px-3 rounded-lg'>
+                    Delete
+                </button>
+            </div>
         </div>
     </div>
 )
